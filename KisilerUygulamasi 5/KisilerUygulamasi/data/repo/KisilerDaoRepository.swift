00b581453aa823191880622b2f1ae2582140065c
Ceren Acay <cerenacay35@gmail.com>
2024-09-02 23:39:49 +0300

    Kisiler Uygulaması Core Data

---
+++ KisilerUygulamasi 5/KisilerUygulamasi/data/repo/KisilerDaoRepository.swift
@@ -0,0 +1,58 @@
+//
+//  KisilerDaoRepository.swift
+//  KisilerUygulamasi
+//
+//  Created by Kasım on 9.08.2023.
+//
+
+import Foundation
+import RxSwift
+import CoreData
+
+class KisilerDaoRepository {
+    var kisilerListesi = BehaviorSubject<[KisilerModel]>(value: [KisilerModel]())
+    
+    let context = appDelegate.persistentContainer.viewContext
+    
+    func kaydet(kisi_ad:String,kisi_tel:String){
+        let kisi = KisilerModel(context: context)
+        kisi.kisi_ad = kisi_ad
+        kisi.kisi_tel = kisi_tel
+        
+        appDelegate.saveContext()
+    }
+    
+    func guncelle(kisi:KisilerModel,kisi_ad:String,kisi_tel:String){
+        kisi.kisi_ad = kisi_ad
+        kisi.kisi_tel = kisi_tel
+        
+        appDelegate.saveContext()
+    }
+    
+    func sil(kisi:KisilerModel){
+        context.delete(kisi)
+        appDelegate.saveContext()
+        kisileriYukle()
+    }
+    
+    func ara(aramaKelimesi:String){
+        do{
+            let fr = KisilerModel.fetchRequest()
+            fr.predicate = NSPredicate(format: "kisi_ad CONTAINS[c] %@", aramaKelimesi)
+            
+            let liste = try context.fetch(fr)
+            kisilerListesi.onNext(liste)//Tetikleme
+        }catch{
+            print(error.localizedDescription)
+        }
+    }
+    
+    func kisileriYukle(){
+        do{
+            let liste = try context.fetch(KisilerModel.fetchRequest())
+            kisilerListesi.onNext(liste)//Tetikleme
+        }catch{
+            print(error.localizedDescription)
+        }
+    }
+}
