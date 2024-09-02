d58886d362ee4c007ecd8ab89d266e1b4d1997ae
Ceren Acay <cerenacay35@gmail.com>
2024-09-02 20:22:57 +0300

    Tasarım Calismasi-AutoLayout-Align-Pin-Constraint-Çoklu Ekran ve Dil Desteği-StackView

---
+++ BenimTasarimim/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BenimTasarimim
+//
+//  Created by Ceren Acay on 22.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Waffle"
+        
+        let appearence = UINavigationBarAppearance()
+        
+        appearence.backgroundColor = UIColor(named: "anaRenk")
+        
+        appearence.titleTextAttributes = [ .foregroundColor:UIColor(named: "yaziRenk1")! , .font:UIFont(name: "Pacifico-Regular", size: 22) ]
+        
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearence
+        navigationController?.navigationBar.compactAppearance = appearence
+        navigationController?.navigationBar.scrollEdgeAppearance = appearence
+    }
+
+
+}
+
