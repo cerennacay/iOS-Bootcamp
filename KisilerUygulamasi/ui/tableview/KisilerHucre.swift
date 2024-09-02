0b13bf3c94c47d33e6b73b01377eb7a66b70786c
Ceren Acay <cerenacay35@gmail.com>
2024-09-02 20:45:12 +0300

    Kisiler Uygulaması

---
+++ KisilerUygulamasi/ui/tableview/KisilerHucre.swift
@@ -0,0 +1,27 @@
+//
+//  KisilerHucre.swift
+//  KisilerUygulamasi
+//
+//  Created by Ceren Acay on 28.08.2024.
+//
+
+import UIKit
+
+class KisilerHucre: UITableViewCell {
+    
+    @IBOutlet weak var lblKisiAd: UILabel!
+    
+    @IBOutlet weak var lblKisiTel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
