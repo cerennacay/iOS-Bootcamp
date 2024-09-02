5dcc00984862484681f862e84e3de3ba666abe88
Ceren Acay <cerenacay35@gmail.com>
2024-09-02 20:43:36 +0300

    Scroll View

---
+++ ScrollViewKullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewKullanimi
+//
+//  Created by Ceren Acay on 28.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
